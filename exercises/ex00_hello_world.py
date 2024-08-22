4088296160df11b9e5e32f0438f3dc24320c97c4
jacapa <jacapa@unc.edu>
2024-08-22 16:39:01 -0400

    finished activity EX00

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,11 @@
+"""My first exercise in COMP110!"""
+
+__author__ = "730767778"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
+
+
+greet(name="JP")
